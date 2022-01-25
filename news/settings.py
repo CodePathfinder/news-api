07a2a507f503e7aeb75c4ff024dc8d9e4d151a67
Gennadiy Molchanov <gmolchanov@gmail.com>
2022-01-25 19:26:23 +0200

    staticfiles_dir added

--- news/settings.py
+++ news/settings.py
@@ -142,6 +142,10 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'schemas/static'),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
