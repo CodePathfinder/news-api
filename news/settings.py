93023b872664eed45cf0f27c08f624f25fa25c80
Gennadiy Molchanov <gmolchanov@gmail.com>
2022-01-25 19:37:38 +0200

    dependancy update

--- news/settings.py
+++ news/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'schemas/static'),
+    os.path.join(BASE_DIR, 'posts/static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
